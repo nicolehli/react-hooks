8229fe5d2d388b7d996250e13ff2bc5cfbb8322a
Nicole Li <nicolehcli@gmail.com>
2019-02-22 15:54:10 -0800

    Remove comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-// After Refactored
 const App = () => {
   // Use userState hook, no longer need class or setState
   const [lat, setLat] = useState(null);
@@ -31,33 +30,4 @@ const App = () => {
   )
 };
 
-// Before Refactored
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
-}
-
-// Kept before and after refactored
 ReactDOM.render(<App />, document.querySelector('#root'));
